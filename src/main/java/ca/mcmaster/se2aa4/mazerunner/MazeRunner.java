aa7565f723facda7d1bb32acc10228f0a38512a7
JackEaston4 <jack.easton@gmail.com>
2025-01-23 18:34:01 -0500

    removed redundant code

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
@@ -8,15 +8,11 @@ import org.apache.logging.log4j.Logger;
 
 public class MazeRunner{
     
-    private int[] position = new int[2];//change at some point
-    private int[] finish = new int[2];//change at some point
+    private int[] position = new int[2];
+    private int[] finish = new int[2];
+
     private String[][] mazeArray;
     private int[] facing;
-
-    private boolean facingNorth = false;
-    private boolean facingSouth = false;
-    private boolean facingEast = false;
-    private boolean facingWest = true;
 
     public MazeRunner(String[][] mazeArray, int[] entryPoint, int[] finish){
         position = entryPoint;
@@ -24,9 +20,9 @@ public class MazeRunner{
         this.mazeArray = mazeArray;
     }
 
-    public MazeRunnerAlgorithm(String[][] mazeArray){
+    public void MazeRunnerAlgorithm(String[][] mazeArray){
         while(!isFinish(position)){
-            
+            // do stuff
         }
     }
 
@@ -75,90 +71,6 @@ public class MazeRunner{
         }
         else {
             return false;
-        }
-    }
-
-
-    public int[] peekLeft(int[] position){
-        if(facingNorth){
-            return new int[]{position[0],position[1]-1};
-        }
-        else if(facingSouth){
-            return new int[]{position[0],position[1]+1};
-        }
-        else if(facingEast){
-            return new int[]{position[0]+1,position[1]};
-        }
-        else if(facingWest){
-            return new int[]{position[0]-1,position[1]};
-        }
-    }
-
-    public int[] peekRight(int[] position){
-        if(facingNorth){
-            return new int[]{position[0],position[1]+1};
-        }
-        else if(facingSouth){
-            return new int[]{position[0],position[1]-1};
-        }
-        else if(facingEast){
-            return new int[]{position[0]-1,position[1]};
-        }
-        else if(facingWest){
-            return new int[]{position[0]+1,position[1]};
-        }
-    }
-
-    public void moveForward(){
-        if(facingNorth){
-            position[0]++;
-        }
-        else if(facingSouth){
-            position[0]--;
-        }
-        else if(facingEast){
-            position[1]--;
-        }
-        else if(facingWest){
-            position[1]++;
-        }
-    }
-
-    public void turnLeft(){
-        if(facingNorth){
-            facingWest = true;
-            facingNorth = false;
-        }
-        else if(facingSouth){
-            facingEast = true;
-            facingSouth = false;
-        }
-        else if(facingEast){
-            facingNorth = true;
-            facingEast = false;
-        }
-        else if(facingWest){
-            facingSouth = true;
-            facingWest = false;
-        }
-    }
-
-    public void turnRight(){
-        if(facingNorth){
-            facingEast = true;
-            facingNorth = false;
-        }
-        else if(facingSouth){
-            facingWest = true;
-            facingSouth = false;
-        }
-        else if(facingEast){
-            facingSouth = true;
-            facingEast = false;
-        }
-        else if(facingWest){
-            facingNorth = true;
-            facingWest = false;
         }
     }
 
